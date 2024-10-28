15c1594de18c7ac02027a87bcf3dc339c68d806f
tanghai <egametang@gmail.com>
2024-10-28 14:02:16 +0800

    导入主角模型，成功加载进入场景

--- Packages/cn.etetet.wow/Scripts/HotfixView/Client/Scene/SceneChangeStart_AddComponent.cs
+++ Packages/cn.etetet.wow/Scripts/HotfixView/Client/Scene/SceneChangeStart_AddComponent.cs
@@ -15,7 +15,7 @@ namespace ET.Client
                 ResourcesLoaderComponent resourcesLoaderComponent = currentScene.GetComponent<ResourcesLoaderComponent>();
             
                 // 加载场景资源
-                await resourcesLoaderComponent.LoadSceneAsync($"Packages/cn.etetet.wow/Scenes/{currentScene.Name}.unity", LoadSceneMode.Single);
+                await resourcesLoaderComponent.LoadSceneAsync($"Packages/cn.etetet.wow/Bundles/Scenes/{currentScene.Name}.unity", LoadSceneMode.Single);
                 // 切换到map场景
 
                 //await SceneManager.LoadSceneAsync(currentScene.Name);
--- Packages/cn.etetet.wow/Scripts/HotfixView/Client/Unit/AfterUnitCreate_CreateUnitView.cs
+++ Packages/cn.etetet.wow/Scripts/HotfixView/Client/Unit/AfterUnitCreate_CreateUnitView.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.Mathematics;
+using UnityEngine;
 
 namespace ET.Client
 {
@@ -9,15 +10,16 @@ namespace ET.Client
         {
             Unit unit = args.Unit;
             // Unit View层
-            string assetsName = $"Packages/cn.etetet.demores/Bundles/Unit/Unit.prefab";
+            string assetsName = $"Packages/cn.etetet.wow/Bundles/Units/Character_HumanMale.prefab";
             GameObject bundleGameObject = await scene.GetComponent<ResourcesLoaderComponent>().LoadAssetAsync<GameObject>(assetsName);
-            GameObject prefab = bundleGameObject.Get<GameObject>("Skeleton");
 
             GlobalComponent globalComponent = scene.Root().GetComponent<GlobalComponent>();
-            GameObject go = UnityEngine.Object.Instantiate(prefab, globalComponent.Unit, true);
+            GameObject go = UnityEngine.Object.Instantiate(bundleGameObject, globalComponent.Unit, true);
             go.transform.position = unit.Position;
             unit.AddComponent<GameObjectComponent>().GameObject = go;
             unit.AddComponent<AnimatorComponent>();
+
+            unit.Position = new float3(127, 51.3f, -155);
             await ETTask.CompletedTask;
         }
     }
