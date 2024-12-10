7cfd1357369f810961925dc0961102754f796087
Lsy <379338943@qq.com>
2024-12-10 16:26:27 +0800

    消息飘字提示

---
+++ Packages/cn.etetet.wow/Scripts/HotfixView/Client/YIUISystem/Main/Event/M2C_ErrorViewHandler.cs
@@ -0,0 +1,22 @@
+﻿namespace ET.Client
+{
+    [MessageHandler(SceneType.WOW)]
+    public class M2C_ErrorViewHandler : MessageHandler<Scene, M2C_Error>
+    {
+        protected override async ETTask Run(Scene root, M2C_Error message)
+        {
+            TipsHelper.OpenSync<TipsTextViewComponent>($"<color=red>{OutputText(message.Error, message.Values.ToArray())}</color>");
+            await ETTask.CompletedTask;
+        }
+
+        private static string OutputText(int key, params string[] args)
+        {
+            return string.Format(GetText(key), args);
+        }
+
+        private static string GetText(int key)
+        {
+            return TextConfigCategory.Instance.Get(key).CN;
+        }
+    }
+}
