9c651f6b48ccfbe970ab885893d5d82eb3044e6a
tanghai <egametang@gmail.com>
2024-12-10 21:19:30 +0800

    调整SpellMod的实现

--- Packages/cn.etetet.wow/Scripts/Hotfix/Share/Map/Spell/SpellComponentSystem.cs
+++ Packages/cn.etetet.wow/Scripts/Hotfix/Share/Map/Spell/SpellComponentSystem.cs
@@ -35,22 +35,48 @@ namespace ET
             self.CDTime = timeNow;
             self.SpellCD[spellConfigId] = timeNow;
         }
+
+        public static int GetMod(this SpellComponent self, int spellConfigId, SpellModType spellModType)
+        {
+            if (!self.SpellMods.TryGetValue(spellConfigId, out Dictionary<int, int> mods))
+            {
+                return 0;
+            }
+            return mods.GetValueOrDefault((int)spellModType, 0);
+        }
         
         public static void AddMod(this SpellComponent self, int spellConfigId, SpellModType spellModType, int value)
         {
+            if (value == 0)
+            {
+                return;
+            }
+            
             if (!self.SpellMods.TryGetValue(spellConfigId, out Dictionary<int, int> mods))
             {
                 mods = new Dictionary<int, int>();
                 self.SpellMods[spellConfigId] = mods;
             }
 
-            if (mods.TryGetValue((int)spellModType, out int oldValue))
+
+            int modType = (int)spellModType;
+            if (!mods.TryGetValue(modType, out int oldValue))
             {
-                mods[(int)spellModType] = oldValue + value;
+                mods[modType] = value;
+                return;
             }
-            else
+            
+            value = oldValue + value;
+            if (value != 0)
             {
-                mods[(int)spellModType] = value;
+                mods[modType] = value;
+                return;
+            }
+
+            mods.Remove(modType);
+            if (mods.Count == 0)
+            {
+                self.SpellMods.Remove(spellConfigId);
             }
         }
     }
--- Packages/cn.etetet.wow/Scripts/Model/Share/Map/Spell/SpellComponent.cs
+++ Packages/cn.etetet.wow/Scripts/Model/Share/Map/Spell/SpellComponent.cs
@@ -6,35 +6,35 @@ namespace ET
 {
     public enum SpellModType
     {
-        SPELLMOD_DAMAGE                    = 110,  // 伤害
-        SPELLMOD_DURATION                  = 111,  // 持续时间
-        SPELLMOD_THREAT                    = 112,  // 仇恨
-        SPELLMOD_EFFECT1                   = 113,  // 
-        SPELLMOD_CHARGES                   = 114,
-        SPELLMOD_RANGE                     = 115,  // 范围
-        SPELLMOD_RADIUS                    = 116,  // 半径
-        SPELLMOD_CRITICAL_CHANCE           = 117,  
-        SPELLMOD_ALL_EFFECTS               = 118,
-        SPELLMOD_NOT_LOSE_CASTING_TIME     = 119,  // 不减少施放时间
-        SPELLMOD_CASTING_TIME              = 120,  // 施放时间
-        SPELLMOD_COOLDOWN                  = 121,  // 冷却
-        SPELLMOD_IGNORE_ARMOR              = 123,  // 忽略护甲
-        SPELLMOD_COST                      = 124,  // 消耗
-        SPELLMOD_CRIT_DAMAGE_BONUS         = 125,  // 暴击伤害增加
-        SPELLMOD_RESIST_MISS_CHANCE        = 126,  // 抗性
-        SPELLMOD_JUMP_TARGETS              = 127,  //
-        SPELLMOD_CHANCE_OF_SUCCESS         = 128,  //
-        SPELLMOD_ACTIVATION_TIME           = 129,  
-        SPELLMOD_DAMAGE_MULTIPLIER         = 130,
-        SPELLMOD_GLOBAL_COOLDOWN           = 131,  // 全局CD
-        SPELLMOD_DOT                       = 132,
-        SPELLMOD_EFFECT3                   = 133,
-        SPELLMOD_BONUS_MULTIPLIER          = 134,
-        SPELLMOD_PROC_PER_MINUTE           = 136,
-        SPELLMOD_VALUE_MULTIPLIER          = 137,
-        SPELLMOD_RESIST_DISPEL_CHANCE      = 138,
-        SPELLMOD_CRIT_DAMAGE_BONUS_2       = 139, // 暴击伤害增加2
-        SPELLMOD_SPELL_COST_REFUND_ON_FAIL = 140
+        SPELLMOD_DAMAGE                    = 10,  // 伤害
+        SPELLMOD_DURATION                  = 11,  // 持续时间
+        SPELLMOD_THREAT                    = 12,  // 仇恨
+        SPELLMOD_EFFECT1                   = 13,  // 
+        SPELLMOD_CHARGES                   = 14,
+        SPELLMOD_RANGE                     = 15,  // 范围
+        SPELLMOD_RADIUS                    = 16,  // 半径
+        SPELLMOD_CRITICAL_CHANCE           = 17,  
+        SPELLMOD_ALL_EFFECTS               = 18,
+        SPELLMOD_NOT_LOSE_CASTING_TIME     = 19,  // 不减少施放时间
+        SPELLMOD_CASTING_TIME              = 20,  // 施放时间
+        SPELLMOD_COOLDOWN                  = 21,  // 冷却
+        SPELLMOD_IGNORE_ARMOR              = 23,  // 忽略护甲
+        SPELLMOD_COST                      = 24,  // 消耗
+        SPELLMOD_CRIT_DAMAGE_BONUS         = 25,  // 暴击伤害增加
+        SPELLMOD_RESIST_MISS_CHANCE        = 26,  // 抗性
+        SPELLMOD_JUMP_TARGETS              = 27,  //
+        SPELLMOD_CHANCE_OF_SUCCESS         = 28,  //
+        SPELLMOD_ACTIVATION_TIME           = 29,  
+        SPELLMOD_DAMAGE_MULTIPLIER         = 30,
+        SPELLMOD_GLOBAL_COOLDOWN           = 31,  // 全局CD
+        SPELLMOD_DOT                       = 32,
+        SPELLMOD_EFFECT3                   = 33,
+        SPELLMOD_BONUS_MULTIPLIER          = 34,
+        SPELLMOD_PROC_PER_MINUTE           = 36,
+        SPELLMOD_VALUE_MULTIPLIER          = 37,
+        SPELLMOD_RESIST_DISPEL_CHANCE      = 38,
+        SPELLMOD_CRIT_DAMAGE_BONUS_2       = 39, // 暴击伤害增加2
+        SPELLMOD_SPELL_COST_REFUND_ON_FAIL = 40
     };
     
     [ComponentOf(typeof(Unit))]
