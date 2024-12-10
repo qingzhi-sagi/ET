28a849644f5b9fd83ccc9ec8a7700b0aab3605f4
tanghai <egametang@gmail.com>
2024-12-10 23:53:29 +0800

    暴风雪施放后自动转向目标区域

---
+++ Packages/cn.etetet.wow/Scripts/Model/Share/Map/Spell/BT/BTTurnToPos.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Mathematics;
+
+namespace ET
+{
+    public class BTTurnToPos: BTNode
+    {
+        [Sirenix.OdinInspector.BoxGroup("输入参数")]
+        [BTInput(typeof(Unit))]
+        public string Caster;
+        
+        [Sirenix.OdinInspector.BoxGroup("输入参数")]
+        [BTInput(typeof(float3))]
+        public string Pos;
+        
+        [Sirenix.OdinInspector.BoxGroup("输入参数")]
+        [BTInput(typeof(Buff))]
+        public string Buff;
+    }
+}
