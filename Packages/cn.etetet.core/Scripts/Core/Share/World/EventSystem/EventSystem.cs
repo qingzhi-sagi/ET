b5194e08e9fa08c7c714ad48c27b8abb97068483
tanghai <egametang@gmail.com>
2025-06-20 18:23:59 +0800

    去掉EventSystem的patial，提供一个GetInvoker方法给第三方使用

--- Packages/cn.etetet.core/Scripts/Core/Share/World/EventSystem/EventSystem.cs
+++ Packages/cn.etetet.core/Scripts/Core/Share/World/EventSystem/EventSystem.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace ET
 {
     [CodeProcess]
-    public partial class EventSystem: Singleton<EventSystem>, ISingletonAwake
+    public class EventSystem: Singleton<EventSystem>, ISingletonAwake
     {
         private class EventInfo
         {
@@ -147,6 +147,24 @@ namespace ET
             }
         }
         
+        public Invoker GetInvoker<Invoker, A>(long type) where Invoker : class, IInvoke where A: struct
+        {
+            if (!this.allInvokers.TryGetValue(typeof(A), out Dictionary<long, object> invokeHandlers))
+            {
+                return null;
+            }
+            if (!invokeHandlers.TryGetValue(type, out object invokeHandler))
+            {
+                return null;
+            }
+
+            if (invokeHandler is not Invoker aInvokeHandler)
+            {
+                return null;
+            }
+            return aInvokeHandler;
+        }
+        
         // Invoke跟Publish的区别(特别注意)
         // Invoke类似函数，必须有被调用方，否则异常，调用者跟被调用者属于同一模块，比如MoveComponent中的Timer计时器，调用跟被调用的代码均属于移动模块
         // 既然Invoke跟函数一样，那么为什么不使用函数呢? 因为有时候不方便直接调用，比如Config加载，在客户端跟服务端加载方式不一样。比如TimerComponent需要根据Id分发
@@ -154,42 +172,21 @@ namespace ET
         // publish是事件，抛出去可以没人订阅，调用者跟被调用者属于两个模块，比如任务系统需要知道道具使用的信息，则订阅道具使用事件
         public void Invoke<A>(long type, A args) where A: struct
         {
-            if (!this.allInvokers.TryGetValue(typeof(A), out var invokeHandlers))
-            {
-                throw new Exception($"Invoke error1: {type} {typeof(A).FullName}");
-            }
-            if (!invokeHandlers.TryGetValue(type, out var invokeHandler))
-            {
-                throw new Exception($"Invoke error2: {type} {typeof(A).FullName}");
-            }
-
-            var aInvokeHandler = invokeHandler as AInvokeHandler<A>;
+            var aInvokeHandler = this.GetInvoker<AInvokeHandler<A>, A>(type);
             if (aInvokeHandler == null)
             {
-                throw new Exception($"Invoke error3, not AInvokeHandler: {type} {typeof(A).FullName}");
+                throw new Exception($"Invoke error, not AInvokeHandler: {type} {typeof(A).FullName}");
             }
-            
             aInvokeHandler.Handle(args);
         }
         
         public T Invoke<A, T>(long type, A args) where A: struct
         {
-            if (!this.allInvokers.TryGetValue(typeof(A), out var invokeHandlers))
-            {
-                throw new Exception($"Invoke error4: {type} {typeof(A).FullName}");
-            }
-            
-            if (!invokeHandlers.TryGetValue(type, out var invokeHandler))
-            {
-                throw new Exception($"Invoke error5: {type} {typeof(A).FullName}");
-            }
-
-            var aInvokeHandler = invokeHandler as AInvokeHandler<A, T>;
+            var aInvokeHandler = this.GetInvoker<AInvokeHandler<A, T>, A>(type);
             if (aInvokeHandler == null)
             {
-                throw new Exception($"Invoke error6, not AInvokeHandler: {type} {typeof(A).FullName} {typeof(T).FullName} ");
+                throw new Exception($"Invoke error, not AInvokeHandler: {type} {typeof(A).FullName}");
             }
-            
             return aInvokeHandler.Handle(args);
         }
         
@@ -201,6 +198,41 @@ namespace ET
         public T Invoke<A, T>(A args) where A: struct
         {
             return Invoke<A, T>(0, args);
+        }
+        
+        // Invoke跟Publish的区别(特别注意)
+        // Invoke类似函数，必须有被调用方，否则异常，调用者跟被调用者属于同一模块，比如MoveComponent中的Timer计时器，调用跟被调用的代码均属于移动模块
+        // 既然Invoke跟函数一样，那么为什么不使用函数呢? 因为有时候不方便直接调用，比如Config加载，在客户端跟服务端加载方式不一样。比如TimerComponent需要根据Id分发
+        // 注意，不要把Invoke当函数使用，这样会造成代码可读性降低，能用函数不要用Invoke
+        // publish是事件，抛出去可以没人订阅，调用者跟被调用者属于两个模块，比如任务系统需要知道道具使用的信息，则订阅道具使用事件
+        public void TryInvoke<A>(long type, A args) where A: struct
+        {
+            var aInvokeHandler = this.GetInvoker<AInvokeHandler<A>, A>(type);
+            if (aInvokeHandler == null)
+            {
+                return;
+            }
+            aInvokeHandler.Handle(args);
+        }
+        
+        public T TryInvoke<A, T>(long type, A args) where A: struct
+        {
+            var aInvokeHandler = this.GetInvoker<AInvokeHandler<A, T>, A>(type);
+            if (aInvokeHandler == null)
+            {
+                return default;
+            }
+            return aInvokeHandler.Handle(args);
+        }
+        
+        public void TryInvoke<A>(A args) where A: struct
+        {
+            TryInvoke(0, args);
+        }
+        
+        public T TryInvoke<A, T>(A args) where A: struct
+        {
+            return TryInvoke<A, T>(0, args);
         }
     }
 }
--- Packages/cn.etetet.yiuiinvoke/Scripts/Core/Share/EventSystem/EventSystem_Check_Invoke.cs
+++ Packages/cn.etetet.yiuiinvoke/Scripts/Core/Share/EventSystem/EventSystem_Check_Invoke.cs
@@ -1,60 +1,28 @@
-﻿namespace ET
+namespace ET
 {
     /// <summary>
     /// 特殊情况下使用的 提前检查一个Invoke是否存在
     /// </summary>
-    public partial class EventSystem
+    public static class EventSystemCheck_Invoke
     {
-        public bool CheckInvoke<A>(long type) where A : struct
+        public static bool CheckInvoke<A>(this EventSystem self, long type) where A : struct
         {
-            if (!this.allInvokers.TryGetValue(typeof(A), out var invokeHandlers))
-            {
-                return false;
-            }
-
-            if (!invokeHandlers.TryGetValue(type, out var invokeHandler))
-            {
-                return false;
-            }
-
-            var aInvokeHandler = invokeHandler as AInvokeHandler<A>;
-            if (aInvokeHandler == null)
-            {
-                return false;
-            }
-
-            return true;
+            return self.GetInvoker<AInvokeHandler<A>, A>(type) != null;
         }
 
-        public bool CheckInvoke<A, T>(long type) where A : struct
+        public static bool CheckInvoke<A, T>(this EventSystem self, long type) where A : struct
         {
-            if (!this.allInvokers.TryGetValue(typeof(A), out var invokeHandlers))
-            {
-                return false;
-            }
-
-            if (!invokeHandlers.TryGetValue(type, out var invokeHandler))
-            {
-                return false;
-            }
-
-            var aInvokeHandler = invokeHandler as AInvokeHandler<A, T>;
-            if (aInvokeHandler == null)
-            {
-                return false;
-            }
-
-            return true;
+            return self.GetInvoker<AInvokeHandler<A, T>, A>(type) != null;
         }
 
-        public bool CheckInvoke<A>() where A : struct
+        public static bool CheckInvoke<A>(this EventSystem self) where A : struct
         {
-            return CheckInvoke<A>(0);
+            return self.CheckInvoke<A>(0);
         }
 
-        public bool CheckInvoke<A, T>() where A : struct
+        public static bool CheckInvoke<A, T>(this EventSystem self) where A : struct
         {
-            return CheckInvoke<A, T>(0);
+            return self.CheckInvoke<A, T>(0);
         }
     }
 }
