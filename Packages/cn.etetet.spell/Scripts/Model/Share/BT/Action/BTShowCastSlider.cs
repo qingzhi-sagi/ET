496c5b23d55a7032f3e58b79f6d4bd4ec5afdb29
tanghai <egametang@gmail.com>
2025-07-07 01:17:30 +0800

    移除BTEvent

--- Packages/cn.etetet.spell/Scripts/Model/Share/BT/Action/BTShowCastSlider.cs
+++ Packages/cn.etetet.spell/Scripts/Model/Share/BT/Action/BTShowCastSlider.cs
@@ -28,4 +28,14 @@ namespace ET
         public OdinUnityObject Icon = new();
 #endif
     }
+    
+    [Module(ModuleName.Spell)]
+    public struct BTEvent_ShowCastSlider
+    {
+        public EntityRef<Unit>   Unit;
+        public EntityRef<Buff>   Buff;
+        public bool   IsIncrease;
+        public string IconName;
+        public string ShowDisplayName;
+    }
 }
