177289f30756621fcc097cd342b23290d9f9303c
tanghai <egametang@gmail.com>
2025-07-07 01:13:21 +0800

    整理了BT节点，放到该放的包中

--- Packages/cn.etetet.spell/Scripts/Model/Client/TargetSelectDispatcher.cs
+++ Packages/cn.etetet.spell/Scripts/Model/Client/TargetSelectDispatcher.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ET
+namespace ET.Client
 {
     [Module(ModuleName.Spell)]
     public class TargetSelectHandlerAttribute: BaseAttribute
