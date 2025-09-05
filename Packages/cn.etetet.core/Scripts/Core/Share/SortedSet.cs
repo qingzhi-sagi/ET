654f162f2bb8503c7892e930d6a8eb2fb3fe71fa
tanghai <egametang@gmail.com>
2025-09-05 11:53:24 +0800

    修复一个GC问题
    SoretedSet里面有个检查，开启会导致装箱。导致GetComponent会产生GC，忽略掉这个检查

--- Packages/cn.etetet.core/Scripts/Core/Share/SortedSet.cs
+++ Packages/cn.etetet.core/Scripts/Core/Share/SortedSet.cs
@@ -2116,6 +2116,7 @@ namespace ET
         /// <summary>Throws an <see cref="ArgumentNullException"/> if <paramref name="argument"/> is null.</summary>
         /// <param name="argument">The reference type argument to validate as non-null.</param>
         /// <param name="paramName">The name of the parameter with which <paramref name="argument"/> corresponds.</param>
+        [Conditional("IGNORE")]
         public static void ThrowIfNull(object argument, string paramName = null)
         {
             if (argument is null)
