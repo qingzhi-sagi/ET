dab3e2813fc798d59e2874b4aad161150e4fd485
tanghai <egametang@gmail.com>
2025-10-30 16:12:01 +0800

    TimerComponent.idGenerator没用了

--- Packages/cn.etetet.core/Scripts/Core/Share/Timer/TimerComponent.cs
+++ Packages/cn.etetet.core/Scripts/Core/Share/Timer/TimerComponent.cs
@@ -41,9 +41,7 @@ namespace ET
         public readonly Queue<long> timeOutTime = new();
 
         public readonly Queue<long> timeOutTimerIds = new();
-
-        public long idGenerator;
-
+        
         // 记录最小时间，不用每次都去MultiMap取第一个值
         public long minTime = long.MaxValue;
     }
