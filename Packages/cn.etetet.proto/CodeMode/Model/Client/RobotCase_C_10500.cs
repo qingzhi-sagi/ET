75ca71d1548b9c4ae09734231df4e512ad7a560c
tanghai <egametang@gmail.com>
2025-11-19 23:59:46 +0800

    去掉测试通过发消息准备数据的方式，统一直接拿到Fiber准备数据

--- Packages/cn.etetet.proto/CodeMode/Model/Client/RobotCase_C_10500.cs
+++ Packages/cn.etetet.proto/CodeMode/Model/Client/RobotCase_C_10500.cs
@@ -3,64 +3,7 @@ using System.Collections.Generic;
 
 namespace ET
 {
-    [MemoryPackable]
-    [Message(Opcode.RobotCase_001_PrepareData_Request)]
-    [ResponseType(nameof(RobotCase_001_PrepareData_Response))]
-    public partial class RobotCase_001_PrepareData_Request : MessageObject, ILocationRequest, IRobotCaseMessage
-    {
-        public static RobotCase_001_PrepareData_Request Create(bool isFromPool = false)
-        {
-            return ObjectPool.Fetch<RobotCase_001_PrepareData_Request>(isFromPool);
-        }
-
-        [MemoryPackOrder(0)]
-        public int RpcId { get; set; }
-        public override void Dispose()
-        {
-            if (!this.IsFromPool)
-            {
-                return;
-            }
-
-            this.RpcId = default;
-
-            ObjectPool.Recycle(this);
-        }
-    }
-
-    [MemoryPackable]
-    [Message(Opcode.RobotCase_001_PrepareData_Response)]
-    public partial class RobotCase_001_PrepareData_Response : MessageObject, ILocationResponse, IRobotCaseMessage
-    {
-        public static RobotCase_001_PrepareData_Response Create(bool isFromPool = false)
-        {
-            return ObjectPool.Fetch<RobotCase_001_PrepareData_Response>(isFromPool);
-        }
-
-        [MemoryPackOrder(0)]
-        public int RpcId { get; set; }
-        [MemoryPackOrder(1)]
-        public int Error { get; set; }
-        [MemoryPackOrder(2)]
-        public string Message { get; set; }
-        public override void Dispose()
-        {
-            if (!this.IsFromPool)
-            {
-                return;
-            }
-
-            this.RpcId = default;
-            this.Error = default;
-            this.Message = default;
-
-            ObjectPool.Recycle(this);
-        }
-    }
-
     public static partial class Opcode
     {
-        public const ushort RobotCase_001_PrepareData_Request = 10501;
-        public const ushort RobotCase_001_PrepareData_Response = 10502;
     }
 }
--- Packages/cn.etetet.robotcase/Scripts/Hotfix/Test/RobotCase_001_CreateRobot_Handler.cs
+++ Packages/cn.etetet.robotcase/Scripts/Hotfix/Test/RobotCase_001_CreateRobot_Handler.cs
@@ -12,14 +12,7 @@ namespace ET.Test
             // robotcase可以直接用subfiber，这样最简单，可以直接拿到机器人的Fiber操作机器人数据，不需要使用消息通信
             Fiber robot = await fiber.CreateFiber(IdGenerater.Instance.GenerateId(), 0, SceneType.Robot, "RobotCase_001");
             // robotcase可以直接用subfiber，这样最简单，可以直接拿到机器人的Fiber操作机器人数据，不需要使用消息通信
-            // 如果需要发送消息给服务端，可以这样做：
-            
-            // 调用机器人的发送消息方法，注意这里是父fiber调用了子fiber的call方法，按理来说，孩子的Call方法返回的时候，同步上下文会变成孩子的上下文
-            // 由于这里父子fiber的同步上下文一样，所以没有问题
-            RobotCase_001_PrepareData_Request prepareData001Request = RobotCase_001_PrepareData_Request.Create();
-            await robot.Root.GetComponent<ClientSenderComponent>().Call(prepareData001Request);
-
-            // 也可以直接访问服务器的数据，直接设置数据
+            // 直接访问服务器的数据，直接设置数据
             string mapName = robot.Root.CurrentScene().Name;
             
             Fiber map = fiber.GetFiber("MapManager").GetFiber(mapName);
