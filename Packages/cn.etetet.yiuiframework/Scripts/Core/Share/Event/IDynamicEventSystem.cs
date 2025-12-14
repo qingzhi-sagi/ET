5b59c9087b57cdcd74cab8f3663b8d6b2f48946d
LiShengYang-yiyi <46582804+LiShengYang-yiyi@users.noreply.github.com>
2025-12-14 18:51:58 +0800

    动态消息无效的问题  (#6)
    ET10 新代码修改了对应AI
    重新基础的类不正确 
    会导致相关消息都没有监听者

--- Packages/cn.etetet.yiuiframework/Scripts/Core/Share/Event/IDynamicEventSystem.cs
+++ Packages/cn.etetet.yiuiframework/Scripts/Core/Share/Event/IDynamicEventSystem.cs
@@ -2,7 +2,11 @@ using System;
 
 namespace ET
 {
+    #if ET10
     public interface IDynamicEvent<A> : IEvent<A> where A : struct
+    #else
+    public interface IDynamicEvent<A> : IClassEvent<A> where A : struct
+    #endif
     {
     }
 
@@ -12,7 +16,11 @@ namespace ET
     }
 
     [EntitySystem]
-    public abstract class DynamicEventSystem<T, A> : SystemObject, IDynamicEventSystem<A>
+    #if ET10
+    public abstract class DynamicEventSystem<T, A> : EventSystem<T, A>, IDynamicEventSystem<A>
+    #else
+    public abstract class DynamicEventSystem<T, A> : ClassEventSystem<T, A>, IDynamicEventSystem<A>
+    #endif
             where T : Entity, IDynamicEvent<A> where A : struct
     {
         Type ISystemType.Type()
@@ -25,10 +33,28 @@ namespace ET
             return typeof(IDynamicEventSystem<A>);
         }
 
+        #if ET10
         public async ETTask Run(Entity o, A message)
         {
             await DynamicEvent((T)o, message);
         }
+
+        protected override void Event(T e, A t)
+        {
+            throw new NotImplementedException();
+        }
+
+        #else
+        public new async ETTask Run(Entity o, A message)
+        {
+            await DynamicEvent((T)o, message);
+        }
+
+        protected override void Handle(Entity e, A t)
+        {
+            throw new NotImplementedException();
+        }
+        #endif
 
         protected abstract ETTask DynamicEvent(T self, A message);
     }
